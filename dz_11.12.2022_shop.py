16b0047049d1938a63982f99ecc0c09488fd4e50
Marina <118795503+MariSheff@users.noreply.github.com>
2022-12-12 17:59:48 +0300

    Add files via upload

---
+++ dz_11.12.2022_shop.py
@@ -0,0 +1,124 @@
+#категория продукта
+class CategoryProduct:
+
+    def __init__(self, category_name):
+        self.category_name = category_name
+
+    def __str__(self):
+        return f'{self.category_name}'
+
+# ТОВАР
+class Product(CategoryProduct):
+
+    def __init__(self, name_product, category, description=None, price=0, count_product=0):
+        self.name_product = name_product
+        self.category = category
+        self.description = description
+        self.price = price
+        self.count_product = count_product #доступное количество на складе
+
+    def __str__(self):
+        return f'{self.description} \n   цена: {self.price}:, в наличии {self.count_product} меш.'
+
+    # в наличии
+    def available_product(self):
+        if self.count_product>0:
+            print(f'{self.name_product} в наличии {self.count_product} меш.')
+            return True
+        else:
+            print(f'{self.name_product} нет в наличии')
+            return False
+
+#корзина клиента
+class ClientCart(Product):
+
+    def __init__(self, count):
+        self.count = count #??? надо убрать?
+        self.total_count = 0
+        self.total_price = 0
+        self.lst_cart = []
+
+    def __str__(self):
+        return f'{self.count}'
+
+#добавление товара в корзину, пересчет остатка товара
+    def add_product(self, product, count):
+        if product.count_product >= count:
+            self.count += count
+            self.lst_cart.append(product.name_product)
+            self.total_price += self.count * product.price
+            self.total_count += count
+        else:
+            print(f'{product.name_product} нет в наличии')
+
+#удаление из корзины, нужен пересчет остатка товара
+    def dell_product(self, product, count):
+        self.count -= count
+        self.lst_cart.remove(product.name_product)
+        product.count_product += count
+        self.total_count -= count
+        self.total_price -= count*product.price
+
+#очистка корзины после покупки или клиентом
+    def clearCart(self):
+        self.lst_cart.clear()
+        self.total_count = 0
+        self.total_price = 0
+        print("Корзина пуста")
+
+    def get_goods(self):
+        return self.lst_cart
+
+    def get_total(self):
+        return f'{self.total_count} мешков на общую сумму {self.total_price}'
+
+
+class Client(ClientCart, Product):
+
+    def __init__(self, name):
+        self.name = name
+        self.count_bought = 0
+        self.lst_bought = []
+        self.spend = 0
+
+    def buy(self, product, count):
+       # super().add_product(product, count)
+        if product.count_product >= count:
+            self.count_bought += count
+            self.lst_bought.append([product.name_product, self.count_bought])
+            product.count_product -= self.count_bought
+
+        else:
+            print('Товара нет в наличии')
+
+    def get_goods(self):
+        return self.lst_bought
+
+class HistorySales:
+    pass
+
+
+dry_food = CategoryProduct('Dry food')
+natural_food = CategoryProduct('Natural food')
+
+print(dry_food.__str__())
+print(natural_food.__str__())
+
+savita = Product("SAVITA", dry_food.category_name, 'Корм SAVITA беззерновой корм для взрослых собак с мясом дикого кабана', 5000, 10)
+go = Product("GO!", dry_food.category_name, 'Корм GO! для щенков со свежим ягненком', 6000, 15)
+wolfsblut = Product("Wolfsblut Wild Pacific", dry_food.category_name, 'Дикий океан для взрослых собак с лососем и сельдью', 9000, 20)
+
+print(wolfsblut.__str__()) #описание товара
+print(Product.available_product(go)) # проверка наличия товара
+
+client1 = Client('Mari')
+client2 = Client('Inga')
+client1.buy(wolfsblut,6)
+print(client1.lst_bought)
+
+
+cart1 = ClientCart(0)
+cart1.add_product(wolfsblut, 5)
+print(cart1.__str__())
+
+
